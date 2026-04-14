34c8402d2be7fb89c4e3781ed4a75276fb278c32
bahauddindakua6409-arch <bahauddindakua6409@gmail.com>
2026-04-14 13:03:02 +0600

    Create BootReceiver.java

---
+++ android-native/java/com/nexora/app/BootReceiver.java
@@ -0,0 +1,21 @@
+package com.nexora.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "NexoraBootReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action) ||
+            Intent.ACTION_MY_PACKAGE_REPLACED.equals(action)) {
+            Log.d(TAG, "Boot/update received, FCM will reconnect automatically");
+            new CallNotificationManager(context);
+        }
+    }
+}
